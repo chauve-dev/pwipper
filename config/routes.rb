b307483b35532b6b3079b5659e958a9bee607d7e
revelarr <raph.a.aile@free.fr>
2021-03-25 09:46:11 +0100

    Feat: création de la page dacceuil

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/index', to: "default#index"
   get "/login", to: "login#index"
   get "/login/:id", to: "login#login", as: "connexion"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
