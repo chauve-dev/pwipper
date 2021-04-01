a0639c8e9d09d6bb00fd82d05ff463b7f0d9826f
Joey JEANTET <jeantetjoey.pro@gmail.com>
2021-04-01 09:07:39 +0200

    Première version de la connexion

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,4 +2,9 @@ class LoginController < ApplicationController
   def index
     @Users = Utilisateur.all
   end
+
+  def login
+    session[:current_user_id] = params[:id]
+    redirect_to '/'
+  end
 end
