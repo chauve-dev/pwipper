a3792633e094ebb9e52d427c27d16464962a802e
revelarr <raph.a.aile@free.fr>
2021-04-07 11:53:03 +0200

    Redirection de / sur /login en cas de non authentification

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,6 +1,9 @@
 class DefaultController < ApplicationController
   def index
-    @users = Utilisateur.all
-    redirect_to '/login' if defined?(session[:current_user_id])
+    if session[:current_user_id]
+      @user = Utilisateur.find(session[:current_user_id])
+    else
+      redirect_to '/login'
+    end
   end
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -6,5 +6,6 @@ class LoginController < ApplicationController
   def login
     session[:current_user_id] = params[:id]
     redirect_to '/'
+    return
   end
 end
