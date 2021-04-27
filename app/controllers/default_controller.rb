3c6a6b3a5befc0bdd10c6a662c2f1169225ce90c
Joey JEANTET <jeantetjoey.pro@gmail.com>
2021-04-27 21:03:09 +0200

    Ajout de seeds

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -2,7 +2,12 @@ class DefaultController < ApplicationController
   def index
     if session[:current_user_id]
       @pweep = Pweep.new
-      @pweeps = Pweep.all
+      if request.GET['pourtag'].nil?
+        @pweeps = Pweep.all
+      else
+        @pweeps = Pweep.joins(:pourtag).where(pourtag: {label: request.GET['pourtag']})
+      end
+
       @user = Utilisateur.find(session[:current_user_id])
       @users = Utilisateur.all
       @pourtags = Pourtag.group(:label).order('COUNT(label) DESC').limit(10)
@@ -16,14 +21,13 @@ class DefaultController < ApplicationController
     pourtags_in_pweep = []
     table.each_with_index do | word, index |
       if word.start_with?("%")
-        table[index] = "<a href='test' class='text-pweep-blue'>"+word+"</a>"
+        table[index] = "<a href='?pourtag="+word[1..-1]+"' class='text-pweep-blue'>"+word+"</a>"
         pourtags_in_pweep.push(word)
       end
     end
     pweep = Pweep.create(body: table.join(' '), utilisateur_id: session[:current_user_id])
-    puts "pweep id", pweep.id
     pourtags_in_pweep.each do |pourtag|
-      Pourtag.create(label: pourtag, pweep_id: pweep.id)
+      Pourtag.create(label: pourtag[1..-1], pweep_id: pweep.id)
     end
     redirect_to '/'
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,28 @@
 
 
 
-Ut1 = Utilisateur.create(nickname: "Chauve", username: "baldus_maximus", email: "jeantetjoey.pro@gmail.com", password: "123", bio: "Chauve un jour chauve toujours", profil_picture: nil)
+Ut1 = Utilisateur.create(nickname: "Chauve",
+                         username: "baldus_maximus",
+                         email: "jeantetjoey.pro@gmail.com",
+                         password: "123",
+                         bio: "Chauve un jour chauve toujours",
+                         profil_picture: nil)
+Ut1.avatar.attach(io: File.open('db/img_seed/ppchauve.jpg'), filename: 'ppchauve.jpg')
+
+Ut2 = Utilisateur.create(nickname: "era",
+                         username: "era_dication",
+                         email: "era@gmail.com",
+                         password: "123",
+                         bio: "Pas contente",
+                         profil_picture: nil)
+Ut2.avatar.attach(io: File.open('db/img_seed/era.png'), filename: 'era.png')
+
+Ut3 = Utilisateur.create(nickname: "Solarius",
+                         username: "gre_forever",
+                         email: "solarius@gmail.com",
+                         password: "123",
+                         bio: "J'aime les pâtes",
+                         profil_picture: nil)
+Ut3.avatar.attach(io: File.open('db/img_seed/solarius.jpg'), filename: 'solarius.jpg')
+
 Pt1 = Pweep.create(body: "Mon premier pweep", utilisateur_id: Ut1.id)
