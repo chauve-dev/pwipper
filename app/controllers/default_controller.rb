baa0f74f393f9fec8c5124874b6af23df2b9ffc2
Joey JEANTET <jeantetjoey.pro@gmail.com>
2021-04-29 20:19:16 +0200

    Modification du modèle

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -17,18 +17,7 @@ class DefaultController < ApplicationController
   end
 
   def newpweep
-    table = params.require(:content).split(/ /, -1)
-    pourtags_in_pweep = []
-    table.each_with_index do | word, index |
-      if word.start_with?("%")
-        table[index] = "<a href='?pourtag="+word[1..-1]+"' class='text-pweep-blue'>"+word+"</a>"
-        pourtags_in_pweep.push(word)
-      end
-    end
-    pweep = Pweep.create(body: table.join(' '), utilisateur_id: session[:current_user_id])
-    pourtags_in_pweep.each do |pourtag|
-      Pourtag.create(label: pourtag[1..-1], pweep_id: pweep.id)
-    end
+    Pweep.create(body: params.require(:content), utilisateur_id: session[:current_user_id])
     redirect_to '/'
   end
 end
